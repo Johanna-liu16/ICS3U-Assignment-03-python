6906e9885de76ba911b63f74d90df1186f77b5eb
Johanna Liu <johanna.liu@mths.ca>
2022-10-21 15:25:25 +0000

    add

---
+++ even_odd.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# Created by: Johanna Liu
+# Created on: Oct 2022
+# This program identifies a even or odd number
+
+
+def main():
+    # this function identifies a even or odd number
+
+    # input
+    print("This program identifies a even or odd number.")
+    user_number = int(input("Enter a number: "))
+
+    # process
+    volume = constants.PI * radius**2 * height / 3
+
+    # output
+    print("The volume of the cone is {0} cm³.".format(round(volume, 2)))
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
