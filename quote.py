e1c2a0de73fd07694caed0688d88b3df0d2ae34a
Johanna Liu <johanna.liu@mths.ca>
2022-10-26 15:24:46 +0000

    add

--- quote.py
+++ quote.py
@@ -7,6 +7,7 @@
 from re import sub
 import constants
 
+
 def main():
     # this function calculates the price
 
@@ -16,17 +17,21 @@ def main():
     user_number = input("Enter the amount of units you want to buy: ")
 
     # process
-    sub_total = user_number * constants.UNIT_PRICE
-    plus_tax = sub_total * constants.TAX
-    total = sub_total + plus_tax
-
-    if user_number >= 1000:
-        new_sub_total = total * 0.10
-        new_total = total - new_sub_total
-        print("You received a 10% discount. \nYour total is $ {0}".format(new_total))
-
-    elif user_number <= 1000:
-        print("Your total is ${0}.".format(total))
+    try:
+        integer_as_number = int(user_number)
+    except ValueError:
+        print("Invalid integer")
+        sub_total = user_number * constants.UNIT_PRICE
+        plus_tax = sub_total * constants.TAX
+        total = sub_total + plus_tax
+    finally:
+        print("Thanks for playing")
+        if user_number >= 1000:
+            new_sub_total = total * 0.10
+            new_total = total - new_sub_total
+            print("You received a 10% discount. \nYour total is $ {0}".format(new_total))
+        elif user_number <= 1000:
+            print("Your total is ${0}.".format(total))
     # output
 
     print("\nDone.")
