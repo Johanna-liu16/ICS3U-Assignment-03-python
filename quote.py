1dd5fd8b34508d93de0be9ddc79f838e9c3e8188
Johanna Liu <99354992+Johanna-liu16@users.noreply.github.com>
2022-10-27 20:33:19 -0400

    fix

--- quote.py
+++ quote.py
@@ -4,7 +4,6 @@
 # Created on: Oct 2022
 # This program identifies a even or odd number
 
-from re import sub
 import constants
 
 
@@ -13,26 +12,27 @@ def main():
 
     # input
     print("This program calculates the amount you will pay.")
-    print("One unit is $100.")
+    print("One unit is $100.\n")
     user_number = input("Enter the amount of units you want to buy: ")
 
-    # process
+    # process and output
     try:
-        integer_as_number = int(user_number)
+        integer_number = int(user_number)
+        if integer_number >= 1000:
+            sub_total = integer_number * constants.UNIT_PRICE
+            discount = sub_total - (sub_total * 0.10)
+            plus_tax = discount * constants.TAX
+            total = discount + plus_tax
+            print("You received a 10% discount. \nYour total is $ {0}".format(total))
+        elif integer_number <= 1000:
+            sub_total = integer_number * constants.UNIT_PRICE
+            plus_tax = sub_total * constants.TAX
+            total = sub_total + plus_tax
+            print("Your total is ${0}.".format(total))
     except ValueError:
         print("Invalid integer")
-        sub_total = user_number * constants.UNIT_PRICE
-        plus_tax = sub_total * constants.TAX
-        total = sub_total + plus_tax
     finally:
-        print("Thanks for playing")
-        if user_number >= 1000:
-            new_sub_total = total * 0.10
-            new_total = total - new_sub_total
-            print("You received a 10% discount. \nYour total is $ {0}".format(new_total))
-        elif user_number <= 1000:
-            print("Your total is ${0}.".format(total))
-    # output
+        print("Thank You.")
 
     print("\nDone.")
 
